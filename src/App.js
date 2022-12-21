a01eac9113217be6fb6ffca99e4f894f8b40a29e
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-21 16:28:16 +0400

    install ag grid for react and implement in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import "./App.css";
 import { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 function App() {
   const [csvData, setCsvData] = useState("");
-
+  const columns = [
+    {
+      headerName: "Segment Type",
+      field: "Segment Type",
+    },
+    {
+      headerName: "Segment Description",
+      field: "Segment Description",
+    },
+    {
+      headerName: "Answer",
+      field: "Answer",
+    },
+    {
+      headerName: "Count",
+      field: "Count",
+    },
+    {
+      headerName: "Percentage",
+      field: "Percentage",
+    },
+  ];
   useEffect(() => {
     try {
       const getData = async () => {
@@ -18,7 +42,18 @@ function App() {
     }
   }, []);
 
-  console.log(csvData);
+  const Table = () => {
+    return (
+      <div
+        className="ag-theme-alpine"
+        style={{ height: "100vh", width: "100wh" }}
+      >
+        <AgGridReact rowData={csvData} columnDefs={columns} />
+      </div>
+    );
+  };
+
+  return <Table />;
 }
 
 export default App;
